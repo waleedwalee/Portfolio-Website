61c9379e540dde32fe773ea49ab100bc431c838d
Ali <f2022462014@umt.edu.pk>
2024-09-27 18:28:21 +0500

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,91 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Waleed Abdullah",
+  jobDescription: "Full-Stack Python and React Developer",
+  about: `I am a software engineer specializing in backend development with expertise in Python, Django, MySQL,
+                    and C++. My strengths lie in creating robust, scalable systems, with a keen focus on data handling
+                    and automation. I am passionate about AI and have experience in building intelligent solutions, from
+                    machine learning models to efficient web scrapers. I thrive on solving complex problems and
+                    continuously improving processes through innovative coding practices and cutting-edge technologies.
+  `,
+
+  experience: [
+    {
+      name: "Final Year Project (Object Detection App)",
+      location: "Lahore, Pakistan",
+      startDate: "Feb 2024",
+      endDate: "Aug 2024",
+      description: [
+        "- Developed proficiency in YOLOv8 for real-time object detection and image processing.",
+        "- Enhanced expertise in React Native for mobile app development.",
+        "- Integrated Django backend with machine learning models, ensuring smooth communication between frontend and backend.",
+        "- Gained experience in API creation and integration for handling data flow between the mobile app and the server.",
+      ],
+    },
+
+    {
+      name: "Social Media Marketing Intern",
+      location: "Lahore, Pakistan",
+      startDate: "July 2022",
+      endDate: "Oct 2022",
+      description: [
+        "✓ Gained hands-on experience managing multiple social media accounts for both the company and its clients.",
+        "✓ Developed expertise in using tools like Buffer for scheduling and managing posts across platforms.",
+        "✓ Learned to create and optimize Meta Ads campaigns to increase reach and engagement.",
+        "✓ Enhanced skills in content creation and editing using tools like Adobe Photoshop for designing eye-catching visuals.",
+        "✓ Improved ability to analyze social media performance through various tools, adjusting strategies based on data-driven insights.",
+      ],
+    },
+
+  ],
+
+  education: [
+    {
+      name: "University of Managment and Technology",
+      location: "Lahore, Pakistan",
+      startDate: "2022",
+      endDate: "2024",
+      description: [
+        "Software Engineering",
+      ],
+    },
+    {
+      name: "University of the Punjab",
+      location: "Lahore, Pakistan",
+      startDate: "2018",
+      endDate: "2022",
+      description: ["Bachelor of Managment"],
+    },
+    {
+      name: "Govt. Shalimar College for Boys",
+      location: "Lahore, Pakistan",
+      startDate: "2016",
+      endDate: "2018",
+      description: ["Intermediate,  FSc_Pre-Engineering"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=61566558654292",
+    twitter: "",
+    github: "https://github.com/waleedwalee",
+    email: "abdullahwaleed812@gmail.com",
+    linkedin: "https://www.linkedin.com/in/waleed-abdullah-534b64215/",
+  },
+
+  projects: [
+    {
+      title: "CheckMe - Object Detection App",
+      isFeatured: true,
+      thumbnail: "/assets/images/Checkme.jpeg",
+      githubUrl: "https://github.com/waleedwalee",
+    },
+    {
+      title: "Personal Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/thumbnail.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
